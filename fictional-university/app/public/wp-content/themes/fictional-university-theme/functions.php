749924cf7df9ccb7bbc46e07d55d8de25d932846
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-02-06 15:40:07 +0600

    about us and privacy policy page created and site title created

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/functions.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -8,7 +8,13 @@ function university_files(): void
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('university_main_style', get_theme_file_uri('/build/style-index.css'));
   wp_enqueue_style('university_extra_style', get_theme_file_uri('/build/index.css'));
-
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features(): void
+{
+  add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'university_features');
