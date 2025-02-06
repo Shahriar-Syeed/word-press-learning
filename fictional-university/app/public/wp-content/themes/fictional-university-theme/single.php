3d27a94ad9249386f90f547adf7d49f8e702e3e8
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-02-06 14:25:50 +0600

    convert static html template into wordpress

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/single.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/single.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 get_header();
 
 while (have_posts()) {
   the_post();
-  ?>
+?>
   <h2><?php the_title(); ?></h2>
-  <p><?php the_content();?></p>
+  <p><?php the_content(); ?></p>
 
-  <?php
+<?php
 }
 get_footer();
 ?>
