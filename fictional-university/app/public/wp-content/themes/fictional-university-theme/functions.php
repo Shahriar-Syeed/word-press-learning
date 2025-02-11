7fa8602325c5741a60363809b16274ba6e3a956a
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-02-11 12:34:16 +0600

    navigation menu static with class current page addition

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/functions.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -14,9 +14,9 @@ add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features(): void
 {
-  register_nav_menu('headerMenuLocation', 'Header Menu Location');
-  register_nav_menu('footerMenuLocationOne', 'Footer Menu Location One');
-  register_nav_menu('footerMenuLocationTwo', 'Footer Menu Location Two');
+  // register_nav_menu('headerMenuLocation', 'Header Menu Location');
+  // register_nav_menu('footerMenuLocationOne', 'Footer Menu Location One');
+  // register_nav_menu('footerMenuLocationTwo', 'Footer Menu Location Two');
   add_theme_support('title-tag');
 }
 
