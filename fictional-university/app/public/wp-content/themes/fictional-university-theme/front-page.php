5384bfd67739c23067e688f69be9ae4d4927391e
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-02-12 14:32:19 +0600

    custom query  for front page blog view

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -116,8 +116,33 @@
   <div class="full-width-split__two">
     <div class="full-width-split__inner">
       <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+      <?php
+      $homepagePosts = new WP_Query(array(
+        'post_per_page' => 2,
+        // 'category_name' => 'awards',
+        // 'post_type' => 'page'
+      ));
 
-      <div class="event-summary">
+      while ($homepagePosts->have_posts()) {
+        $homepagePosts->the_post();
+      ?>
+
+        <div class="event-summary">
+          <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+            <span class="event-summary__month"><?php the_time('M'); ?></span>
+            <span class="event-summary__day"><?php the_time('d'); ?></span>
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h5>
+            <p><?php echo wp_trim_words(get_the_content(), 20); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+          </div>
+        </div>
+      <?php
+      }
+      wp_reset_postdata();
+      ?>
+
+      <!-- <div class="event-summary">
         <a class="event-summary__date event-summary__date--beige t-center" href="#">
           <span class="event-summary__month">Jan</span>
           <span class="event-summary__day">20</span>
@@ -136,7 +161,7 @@
           <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
           <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
         </div>
-      </div>
+      </div> -->
 
       <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
     </div>
