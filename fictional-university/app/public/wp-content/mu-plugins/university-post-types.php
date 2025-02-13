a0817b50071e588053eed21a8a5823387fec9caf
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-02-13 18:26:42 +0600

    creating the program post type

--- fictional-university/app/public/wp-content/mu-plugins/university-post-types.php
+++ fictional-university/app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,7 @@
 <?php
 function university_post_type()
 {
+  // event post type
   register_post_type('event', array(
     'show_in_rest' => true,
     'supports' => array(
@@ -21,6 +22,29 @@ function university_post_type()
       'singular_name' => 'Event',
     ),
     'menu_icon' => 'dashicons-calendar',
+  ));
+
+  //Program post type
+  register_post_type('program', array(
+    'show_in_rest' => true,
+    'supports' => array(
+      'title',
+      'editor',
+      // 'excerpt',
+      // 'custom-fields'
+    ),
+    'rewrite' => array('slug' => 'programs'),
+    'has_archive' => true,
+    'public' => true,
+    // 'show_in_reset' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program',
+    ),
+    'menu_icon' => 'dashicons-awards',
   ));
 }
 add_action('init', 'university_post_type');
