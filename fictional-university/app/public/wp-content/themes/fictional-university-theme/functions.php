9c455827da6b63d9c5224ad372d53a3c19d2065f
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-02-13 16:46:55 +0600

    manipulating default URL base query

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/functions.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -33,3 +33,24 @@ add_action('after_setup_theme', 'university_features');
 //   ));
 // }
 // add_action('init', 'university_post_type');
+function university_adjust_query($query)
+{
+  // $query->set('posts_per_page', '1'); //universal
+  if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
+    $today = date('Ymd');
+    // $query->set('posts_per_page', '1');
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+      array(
+        'key' => 'event_date',
+        'compare' => '>=',
+        'value' => $today,
+        'type' => 'numeric',
+      ),
+    ));
+  }
+}
+
+add_action('pre_get_posts', 'university_adjust_query');
