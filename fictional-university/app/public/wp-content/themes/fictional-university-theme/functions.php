7a4cff754b58d79ccf55cb95839a3fbc91f23222
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-02-19 15:35:05 +0600

    create custom size for newly uploaded image

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/functions.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -19,6 +19,8 @@ function university_features(): void
   // register_nav_menu('footerMenuLocationTwo', 'Footer Menu Location Two');
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
+  add_image_size('professorLandscape', 400, 260, true);
+  add_image_size('professorPortrait', 480, 650, true);
 }
 
 add_action('after_setup_theme', 'university_features');
