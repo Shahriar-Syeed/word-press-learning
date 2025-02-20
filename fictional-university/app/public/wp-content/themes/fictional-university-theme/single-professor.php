5e8f29c1ccd2ffe02a285167e4e56befcbcf86cd
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-02-20 12:47:55 +0600

    page banner dynamic background image with custom field and show in single professor

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/single-professor.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/single-professor.php
@@ -3,11 +3,17 @@ get_header();
 
 ?>
 <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
+  <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image');
+                                                                  if ($pageBannerImage) {
+                                                                    echo $pageBannerImage['url'];
+                                                                  } else {
+                                                                    echo get_theme_file_uri('images/ocean.jpg');
+                                                                  } ?>)"></div>
+
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title"><?php the_title(); ?></h1>
     <div class="page-banner__intro">
-      <p>Professor's review</p>
+      <p><?php the_field('page_banner_subtitle'); ?></p>
     </div>
   </div>
 </div>
