dbf275136c201e47f758eea54e36d7c6af4483a7
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-02-25 14:52:38 +0600

    getting our computer and project ready for js

---
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '777139619e6999cf4caa');
--- fictional-university/app/public/wp-content/themes/fictional-university-theme/src/index.js
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/src/index.js
@@ -6,4 +6,5 @@ import HeroSlider from "./modules/HeroSlider"
 
 // Instantiate a new object using our modules/classes
 const mobileMenu = new MobileMenu()
-const heroSlider = new HeroSlider()
+const heroSlider = new HeroSlider();
+alert('Just a test');
