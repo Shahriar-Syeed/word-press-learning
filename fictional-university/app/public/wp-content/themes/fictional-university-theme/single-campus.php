f5c233d7aac43a3b7ced66cbee4cde27348027a1
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-02-26 11:27:33 +0600

    program with related campus

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/single-campus.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/single-campus.php
@@ -19,7 +19,7 @@ pageBanner();
     <h2><?php the_title(); ?></h2>
     <div class="metabox metabox--position-up metabox--with-home-link">
       <p>
-        <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a>
+        <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('campus'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Campuses</a>
         <span class="metabox__main"><?php the_title(); ?></span>
       </p>
     </div>
@@ -28,10 +28,10 @@ pageBanner();
     <?php
     $today = date('Ymd');
 
-    $relatedProfessors = new WP_Query(array(
+    $relatedPrograms = new WP_Query(array(
       'posts_per_page' => -1,
-      'post_type' => 'professor',
-      // 'orderby' => 'title',
+      'post_type' => 'program',
+      'orderby' => 'title',
       // 'orderby' => 'rand',
       // 'meta_key' => 'professor_date',
       'orderby' => 'title',
@@ -39,24 +39,23 @@ pageBanner();
       'order' => 'ASC',
       'meta_query' => array(
         array(
-          'key' => 'related_programs',
+          'key' => 'related_campus',
           'compare' => 'LIKE',
           'value' => '"' .  get_the_ID() . '"',
         ),
       ),
     ));
 
-    if ($relatedProfessors->have_posts()) {
+    if ($relatedPrograms->have_posts()) {
       echo '<hr class="section-break">';
-      echo '<h2 class="headline headline--medium"> ' . get_the_title() . ' Professor(s)</h2>';
-      echo '<ul class="professor-cards">';
-      while ($relatedProfessors->have_posts()) {
-        $relatedProfessors->the_post();
+      echo '<h2 class="headline headline--medium"> ' . get_the_title() . ' Program(s) Available At This Campus</h2>';
+      echo '<ul class="min-list link-list">';
+      while ($relatedPrograms->have_posts()) {
+        $relatedPrograms->the_post();
     ?>
-        <li class="professor-card__list-item">
-          <a class="professor-card" href="<?php echo the_permalink(); ?>">
-            <img src="<?php the_post_thumbnail_url('professorLandscape'); ?>" alt="" class="professor-card__image">
-            <span class="professor-card__name"><?php the_title(); ?></span>
+        <li>
+          <a href="<?php the_permalink(); ?>">
+            <?php the_title(); ?>
           </a>
         </li>
 
@@ -93,7 +92,6 @@ pageBanner();
     if ($homepageEvents->have_posts()) {
       echo '<hr class="section-break">';
       echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Event(s)</h2>';
-
       while ($homepageEvents->have_posts()) {
         $homepageEvents->the_post();
         get_template_part('template-parts\content-event');
@@ -121,23 +119,12 @@ pageBanner();
           </div>
         </div> -->
 
-      <?php
+    <?php
       }
+      wp_reset_postdata();
     }
-    wp_reset_postdata();
-    $relatedCampuses = get_field('related_campus');
 
-    if ($relatedCampuses) {
-      echo '<hr class="section-break">';
-      echo '<h2 class="headline headline--medium">' . get_the_title() . ' is Available At These Campuses:</h2>';
-      echo '<ul class="min-list link-list">';
-      foreach ($relatedCampuses as $campus) {
-      ?> <li><a href="<?php echo get_the_permalink($campus); ?>"><?php echo get_the_title($campus) ?></a></li> <?php
-                                                                                                              }
-                                                                                                              echo '</ul>';
-                                                                                                            }
-
-                                                                                                                ?>
+    ?>
 </div>
 
 <?php
