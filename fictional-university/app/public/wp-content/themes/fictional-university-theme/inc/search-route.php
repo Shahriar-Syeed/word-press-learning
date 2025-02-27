f1456026f7aecd5d8eaddc297ffb4c33b309e274
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-02-27 19:48:35 +0600

    Add dynamic professor query based on related programs in meta query.

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -52,6 +52,7 @@ function universitySearchResults($data)
       array_push($results['programs'], array(
         'title' => get_the_title(),
         'permalink' => get_the_permalink(),
+        'id' => get_the_ID()
       ));
     }
     if (get_post_type() == 'campus') {
@@ -77,28 +78,55 @@ function universitySearchResults($data)
       ));
     }
   }
+  if ($results['programs']) {
+    $programsMetaQuery = array(
+      'relation' => 'OR'
+    );
 
-  $programRelationshipQuery = new WP_Query(array(
-    'post_type' => 'professor',
-    'meta_query' => array(
-      array(
+    foreach ($results["programs"] as $item) {
+      array_push($programsMetaQuery, array(
         'key' => 'related_programs',
         'compare' => 'LIKE',
-        'value' => '"68"',
-      ),
-    ),
-  ));
-
-  while ($programRelationshipQuery->have_posts()) {
-    $programRelationshipQuery->the_post();
-    if (get_post_type() == 'professor') {
-      array_push($results['professors'], array(
-        'title' => get_the_title(),
-        'permalink' => get_the_permalink(),
-        'image' => get_the_post_thumbnail_url(0, 'professorLandscape'),
-      ));
+        'value' => '"' . $item['id'] . '"',
+      ),);
     }
+
+
+    $programRelationshipQuery = new WP_Query(array(
+      'post_type' => 'professor',
+      'meta_query' => $programsMetaQuery,
+      // 'meta_query' => array(
+      //   'relation' => 'OR',
+      //   array(
+      //     'key' => 'related_programs',
+      //     'compare' => 'LIKE',
+      //     'value' => '"' . $results['programs'][0]['id'] . '"',
+      //   ),
+      //   array(
+      //     'key' => 'related_programs',
+      //     'compare' => 'LIKE',
+      //     'value' => '"' . $results['programs'][1]['id'] . '"',
+      //   ),
+      //   array(
+      //     'key' => 'related_programs',
+      //     'compare' => 'LIKE',
+      //     'value' => '"' . $results['programs'][2]['id'] . '"',
+      //   ),
+      // ),
+    ));
+
+    while ($programRelationshipQuery->have_posts()) {
+      $programRelationshipQuery->the_post();
+      if (get_post_type() == 'professor') {
+        array_push($results['professors'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink(),
+          'image' => get_the_post_thumbnail_url(0, 'professorLandscape'),
+        ));
+      }
+    }
+    $results["professors"] = array_values(array_unique($results['professors'], SORT_REGULAR));
   }
-  $results["professors"] = array_values(array_unique($results['professors'], SORT_REGULAR));
+
   return $results;
 }
