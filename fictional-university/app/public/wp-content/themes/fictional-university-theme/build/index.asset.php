84809f38eedc7602eb167da75710136cc4506f4c
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-03-04 13:05:46 +0600

    traditional word press searching if js not working,

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '0ef01494b9f75ba63b31');
+<?php return array('dependencies' => array(), 'version' => '0e3df30d85f440b74a6e');
--- fictional-university/app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -39,6 +39,7 @@
 //     setTimeout(() => 
 //       this.searchField.focus(), 301);
 //     this.isOverlayOpen = true;
+//     return false;
 //   }
 //   closeOverlay(){
 //     this.searchOverlay.removeClass("search-overlay--active");
@@ -181,138 +182,138 @@
 //   }
 // }
 
-// class Search {
-//   // 1. describe and create/initiate our object
-//   constructor(){
-//     // this.name = "Jane";
-//     // this.eyeColor = "green";
-//     // this.head = {};
-//     // this.brain = {};
-//     this.openButton= document.querySelector(".js-search-trigger");
-//     this.closeButton= document.querySelector(".search-overlay__close");
-//     this.searchOverlay = document.querySelector(".search-overlay");
-//     this.events();
-//     this.isOverlayOpen = false;
+// // class Search {
+// //   // 1. describe and create/initiate our object
+// //   constructor(){
+// //     // this.name = "Jane";
+// //     // this.eyeColor = "green";
+// //     // this.head = {};
+// //     // this.brain = {};
+// //     this.openButton= document.querySelector(".js-search-trigger");
+// //     this.closeButton= document.querySelector(".search-overlay__close");
+// //     this.searchOverlay = document.querySelector(".search-overlay");
+// //     this.events();
+// //     this.isOverlayOpen = false;
 
-//   }
-//   // 2. events
-//   // on this.head feels cold, wearsHat
-//   // on this.brain feels hot, goingSwimming
-//   events(){
-//     this.openButton.addEventListener('click', ()=>this.openOverlay());
-//     this.closeButton.addEventListener('click', ()=>this.closeOverlay());
-//     document.addEventListener('keydown', ()=>this.keyPressDispatcher());
-//   }
+// //   }
+// //   // 2. events
+// //   // on this.head feels cold, wearsHat
+// //   // on this.brain feels hot, goingSwimming
+// //   events(){
+// //     this.openButton.addEventListener('click', ()=>this.openOverlay());
+// //     this.closeButton.addEventListener('click', ()=>this.closeOverlay());
+// //     document.addEventListener('keydown', ()=>this.keyPressDispatcher());
+// //   }
 
-//   // 3.methods (function, action ...)
-//   // goingSwimming(){}
-//   // wearsHat(){}
-//   openOverlay(){
-  //     this.searchOverlay.classList.add("search-overlay--active");
-  //     document.querySelector("body").classList.add("search-overlay--active");
-  // this.isOverlayOpen = true;
-  //   }
-  //   closeOverlay(){
-    //     this.searchOverlay.classList.remove("search-overlay--active");
-    //     document.querySelector("body").classList.remove("search-overlay--active");
-    // this.isOverlayOpen = false;
+// //   // 3.methods (function, action ...)
+// //   // goingSwimming(){}
+// //   // wearsHat(){}
+// //   openOverlay(){
+// //       this.searchOverlay.classList.add("search-overlay--active");
+// //       document.querySelector("body").classList.add("search-overlay--active");
+// //   this.isOverlayOpen = true;
+// //     }
+// //     closeOverlay(){
+// //         this.searchOverlay.classList.remove("search-overlay--active");
+// //         document.querySelector("body").classList.remove("search-overlay--active");
+// //     this.isOverlayOpen = false;
 
-//   }
-//   keyPressDispatcher(){
-// console.log(e);
-    // console.log(e.keyCode);
-    // if(e.keyCode === 83 && !this.isOverlayOpen ){
-    //   this.openOverlay();
-    //   console.log('open');
-    // }
-    // if(e.keyCode === 27 && this.isOverlayOpen ){
-    //   this.closeOverlay();
-    //   console.log('close');
-    // }
+// //   }
+// //   keyPressDispatcher(){
+// // console.log(e);
+// //     console.log(e.keyCode);
+// //     if(e.keyCode === 83 && !this.isOverlayOpen ){
+// //       this.openOverlay();
+// //       console.log('open');
+// //     }
+// //     if(e.keyCode === 27 && this.isOverlayOpen ){
+// //       this.closeOverlay();
+// //       console.log('close');
+// //     }
 
-//   }
-// }
-// class Search {
-//   // 1. Describe and create/initiate our object
-//   constructor() {
-//     // Select elements using vanilla JavaScript
-//     this.resultDiv = document.getElementById("search-overlay__result");
-//     this.openButton = document.querySelector(".js-search-trigger");
-//     this.closeButton = document.querySelector(".search-overlay__close");
-//     this.searchOverlay = document.querySelector(".search-overlay");
-//     this.searchField = document.getElementById("search-term");
+// //   }
+// // }
+// // class Search {
+// //   // 1. Describe and create/initiate our object
+// //   constructor() {
+// //     // Select elements using vanilla JavaScript
+// //     this.resultDiv = document.getElementById("search-overlay__result");
+// //     this.openButton = document.querySelector(".js-search-trigger");
+// //     this.closeButton = document.querySelector(".search-overlay__close");
+// //     this.searchOverlay = document.querySelector(".search-overlay");
+// //     this.searchField = document.getElementById("search-term");
 
-//     // Initialize properties
-//     this.isOverlayOpen = false;
-//     this.isSpinnerVisible = false;
-//     this.previousValue = "";
-//     this.typingTimer = null;
+// //     // Initialize properties
+// //     this.isOverlayOpen = false;
+// //     this.isSpinnerVisible = false;
+// //     this.previousValue = "";
+// //     this.typingTimer = null;
 
-//     // Bind event listeners
-//     this.events();
-//   }
+// //     // Bind event listeners
+// //     this.events();
+// //   }
 
-//   // 2. Events
-//   events() {
-//     // Add event listeners using vanilla JavaScript
-//     this.openButton.addEventListener("click", () => this.openOverlay());
-//     this.closeButton.addEventListener("click", () => this.closeOverlay());
-//     document.addEventListener("keydown", (e) => this.keyPressDispatcher(e));
-//     this.searchField.addEventListener("keyup", (e) => this.typingLogic(e));
-//   }
+// //   // 2. Events
+// //   events() {
+// //     // Add event listeners using vanilla JavaScript
+// //     this.openButton.addEventListener("click", () => this.openOverlay());
+// //     this.closeButton.addEventListener("click", () => this.closeOverlay());
+// //     document.addEventListener("keydown", (e) => this.keyPressDispatcher(e));
+// //     this.searchField.addEventListener("keyup", (e) => this.typingLogic(e));
+// //   }
 
-//   // 3. Methods (functions, actions...)
-//   openOverlay() {
-//     this.searchOverlay.classList.add("search-overlay--active");
-//     document.body.classList.add("body-no-scroll");
-//     this.isOverlayOpen = true;
-//   }
+// //   // 3. Methods (functions, actions...)
+// //   openOverlay() {
+// //     this.searchOverlay.classList.add("search-overlay--active");
+// //     document.body.classList.add("body-no-scroll");
+// //     this.isOverlayOpen = true;
+// //   }
 
-//   closeOverlay() {
-//     this.searchOverlay.classList.remove("search-overlay--active");
-//     document.body.classList.remove("body-no-scroll");
-//     this.isOverlayOpen = false;
-//   }
+// //   closeOverlay() {
+// //     this.searchOverlay.classList.remove("search-overlay--active");
+// //     document.body.classList.remove("body-no-scroll");
+// //     this.isOverlayOpen = false;
+// //   }
 
-//   keyPressDispatcher(e) {
-//     // Open overlay if 'S' key is pressed and no input/textarea is focused
-//     if (e.keyCode === 83 && !this.isOverlayOpen && !document.activeElement.matches("input, textarea")) {
-//       this.openOverlay();
-//       console.log("open");
-//     }
+// //   keyPressDispatcher(e) {
+// //     // Open overlay if 'S' key is pressed and no input/textarea is focused
+// //     if (e.keyCode === 83 && !this.isOverlayOpen && !document.activeElement.matches("input, textarea")) {
+// //       this.openOverlay();
+// //       console.log("open");
+// //     }
 
-//     // Close overlay if 'Esc' key is pressed
-//     if (e.keyCode === 27 && this.isOverlayOpen) {
-//       this.closeOverlay();
-//       console.log("close");
-//     }
-//   }
+// //     // Close overlay if 'Esc' key is pressed
+// //     if (e.keyCode === 27 && this.isOverlayOpen) {
+// //       this.closeOverlay();
+// //       console.log("close");
+// //     }
+// //   }
 
-//   typingLogic(e) {
-//     if (this.searchField.value !== this.previousValue) {
-//       clearTimeout(this.typingTimer);
+// //   typingLogic(e) {
+// //     if (this.searchField.value !== this.previousValue) {
+// //       clearTimeout(this.typingTimer);
 
-//       if (this.searchField.value) {
-//         if (!this.isSpinnerVisible) {
-//           this.resultDiv.innerHTML = '<div class="spinner-loader"></div>';
-//           this.isSpinnerVisible = true;
-//         }
+// //       if (this.searchField.value) {
+// //         if (!this.isSpinnerVisible) {
+// //           this.resultDiv.innerHTML = '<div class="spinner-loader"></div>';
+// //           this.isSpinnerVisible = true;
+// //         }
 
-//         this.typingTimer = setTimeout(() => this.getResult(), 1500);
-//       } else {
-//         this.resultDiv.innerHTML = "";
-//         this.isSpinnerVisible = false;
-//       }
-//     }
+// //         this.typingTimer = setTimeout(() => this.getResult(), 1500);
+// //       } else {
+// //         this.resultDiv.innerHTML = "";
+// //         this.isSpinnerVisible = false;
+// //       }
+// //     }
 
-//     this.previousValue = this.searchField.value;
-//   }
+// //     this.previousValue = this.searchField.value;
+// //   }
 
-//   getResult() {
-//     this.resultDiv.innerHTML = "Imagine real search result here....";
-//     this.isSpinnerVisible = false;
-//   }
-// }
+// //   getResult() {
+// //     this.resultDiv.innerHTML = "Imagine real search result here....";
+// //     this.isSpinnerVisible = false;
+// //   }
+// // }
 
 
 // export default Search;
@@ -448,14 +449,14 @@ class Search {
     }
   }
 
-  openOverlay() {
-    this.searchOverlay.classList.add("search-overlay--active")
-    document.body.classList.add("body-no-scroll")
-    this.searchField.value = ""
-    setTimeout(() => this.searchField.focus(), 301)
+  openOverlay() {;
+    this.searchOverlay.classList.add("search-overlay--active");
+    document.body.classList.add("body-no-scroll");
+    this.searchField.value = "";
+    setTimeout(() => this.searchField.focus(), 301);
     console.log("our open method just ran!")
-    this.isOverlayOpen = true
-    return false
+    this.isOverlayOpen = true;
+    return false;
   }
 
   closeOverlay() {
