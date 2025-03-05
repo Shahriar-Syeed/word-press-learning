035d3a54f86e05c1aeb5311dabc326d0d1bdb92e
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-03-05 13:10:32 +0600

    creating new notes

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '88ac86498fdf970615b0');
+<?php return array('dependencies' => array('jquery'), 'version' => '0432822d2524e7adfeac');
