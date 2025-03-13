e085594d027f4373b0235fd6828edef9bc66c814
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-03-13 17:56:15 +0600

    actually counting words, characters and read time and shown as settings location

--- fictional-university/app/public/wp-content/plugins/word-count-sha-plugin/word-count-sha.php
+++ fictional-university/app/public/wp-content/plugins/word-count-sha-plugin/word-count-sha.php
@@ -15,6 +15,38 @@ class WordCountAndTimePlugin
   {
     add_action('admin_menu', array($this, 'adminPage'));
     add_action('admin_init', array($this, 'settings'));
+    add_filter('the_content', array($this, 'ifWrap'));
+  }
+
+  function ifWrap($content)
+  {
+    if (is_main_query() and is_single() and (get_option('wcp_wordcount', '1') or get_option('wcp_characterCount', '1') or get_option('wcp_readTime', '1'))) {
+      return $this->createHTML($content);
+    }
+    return $content;
+  }
+  function createHTML($content)
+  {
+    $html = '<h3>' . esc_html(get_option('wcp_headline', 'Post Statistics')) . '</h3><p>';
+    // get word count once because both word count  and read time will need it.
+    if (get_option('wcp_wordcount', '1') or get_option('wcp_readTime', '1')) {
+
+      $wordCount = str_word_count(strip_tags($content));
+    }
+    if (get_option('wcp_wordcount', '1')) {
+      $html .= 'This post has ' . $wordCount . ' words.<br>';
+    }
+    if (get_option('wcp_characterCount', '1')) {
+      $html .= 'This post has ' . strlen(strip_tags($content)) . ' characters.<br>';
+    }
+    if (get_option('wcp_readTime', '1')) {
+      $html .= 'This post will take about ' . round($wordCount / 225) . ' minute(s) to read.<br>';
+    }
+    $html .= '</p>';
+    if (get_option('wcp_location', '0') == '0') {
+      return $html . $content;
+    }
+    return $content . $html;
   }
 
   function settings()
