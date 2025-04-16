9bcce2aeeed07cedd18ed0e6264bf45f5dfbf2f1
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-04-16 17:11:13 +0600

    loading a list of professors

--- fictional-university/app/public/wp-content/plugins/featured-professor/build/index.asset.php
+++ fictional-university/app/public/wp-content/plugins/featured-professor/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime'), 'version' => '1b984bb44bcf749ddf01');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-data'), 'version' => 'c6f9c9a340beffd95dff');
