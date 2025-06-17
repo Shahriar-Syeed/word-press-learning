c7e55ae716d0af6ff31f2f9f7231e82666714939
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-06-17 18:28:43 +0600

    click count render

--- fictional-university/app/public/wp-content/plugins/interactivity-quiz/build/render.php
+++ fictional-university/app/public/wp-content/plugins/interactivity-quiz/build/render.php
@@ -11,7 +11,13 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 ?>
-<div data-wp-interactive="create-block" data-wp-context='{"clickCount": 20}'>
+<div style="background-color: <?php echo $attributes['bgColor'] ?>;" class="multiple-choice-frontend" data-wp-interactive="create-block" data-wp-context='{"clickCount": 0}'>
 	<p>The button has been clicked <span data-wp-text="context.clickCount"></span> times.</p>
 	<button data-wp-on--click="actions.buttonHandler">Click me</button>
+	<!-- <p><?php echo $attributes['question'] ?></p>
+	<ul>
+		<li>Answer 1</li>
+		<li>Answer 2</li>
+		<li>Answer 3</li>
+	</ul> -->
 </div>
