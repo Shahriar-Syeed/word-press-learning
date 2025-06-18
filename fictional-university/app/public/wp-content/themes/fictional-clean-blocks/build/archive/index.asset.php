044ea5bcaf230aec625d6c33da4304ff2b9b5e96
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-06-18 19:25:57 +0600

    clean block archive and program archive build

---
+++ fictional-university/app/public/wp-content/themes/fictional-clean-blocks/build/archive/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '82f92aee97749c33ec28');
--- fictional-university/app/public/wp-content/themes/fictional-clean-blocks/build/programarchive/index.asset.php
+++ fictional-university/app/public/wp-content/themes/fictional-clean-blocks/build/programarchive/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '1c12ca0bcc9ab4774930');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '8c3513d97f1120b4d0e0');
