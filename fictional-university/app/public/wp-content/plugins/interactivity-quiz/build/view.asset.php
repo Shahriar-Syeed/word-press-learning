03358abb945df6b25404fb02e1ad6ac8b754f5e2
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2025-06-18 14:05:30 +0600

    automatically convert php data into js data

--- fictional-university/app/public/wp-content/plugins/interactivity-quiz/build/view.asset.php
+++ fictional-university/app/public/wp-content/plugins/interactivity-quiz/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'e95f23eb4c918daf0e49', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'db94bc5b7181a5eecc3c', 'type' => 'module');
